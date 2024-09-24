31c0031dba8c9f85cf82b22b1b14662e50e61d04
Yasin Budayoğlu <yasin2003kerem@gmail.com>
2024-09-24 21:48:38 +0300

    update trpc base url

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -11,7 +11,7 @@ const Providers = ({ children }: PropsWithChildren) => {
     const [trpcClient] = useState(() => trpc.createClient({
         links: [
             httpBatchLink({
-                url: 'http://localhost:3000/api/trpc',
+              url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/trpc`
             }),
         ],
     })
